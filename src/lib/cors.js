209c0d73058ded27c29d328ae3f20d74ecd9aac2
sa-master <dsmyzvzfrv@privaterelay.appleid.com>
2026-09-21 23:03:25 +0300

    Update cors.js

--- src/lib/cors.js
+++ src/lib/cors.js
@@ -4,9 +4,16 @@ export function corsHeaders(origin = "*") {
     "Access-Control-Allow-Methods": "GET, POST, PATCH, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
     "Access-Control-Max-Age": "86400",
+    "Vary": "Origin",
   };
 }
 
 export function preflight() {
-  return new Response(null, { status: 204, headers: corsHeaders() });
+  return new Response(null, {
+    status: 204,
+    headers: {
+      ...corsHeaders(),
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
 }
